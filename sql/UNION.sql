045c83b53104f8043194d1afe4c2287700214527
Issabellahcheru <126699395+Issabellahcheru@users.noreply.github.com>
2026-02-27 15:39:31 +0300

    Add files via upload
    UNION  This script helps analysts view both current and archived orders together in a single result set, while still being able to distinguish their source. It’s a foundational query for reporting across multiple data sources

---
+++ sql/UNION.sql
@@ -0,0 +1,31 @@
+select 'orders' as SourceTable,  
+       [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]      
+
+from Sales.Orders
+union
+select 'OrderArchive' as SourceTable,
+       [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime] 
+from Sales.OrdersArchive
+order by OrderID
